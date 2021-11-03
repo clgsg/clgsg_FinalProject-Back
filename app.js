6d76e73b0590fb6686179dcd0452ca95b76877f6
clgsg <clg_listas@yahoo.es>
2021-11-03 08:52:23 +0100

    wip: commented app.js lines that caused server errors

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const morgan = require("morgan");
-const cors = require("cors");		//* Conexión entre back y front
+// const cors = require("cors");		//* Conexión entre back y front
 
-app.use(cors());
+// app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(morgan("dev"));
@@ -15,11 +15,11 @@ const db = require("./config/db");
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
-app.use(require("./services")(db));
+// app.use(require("./services")(db));
 
-app.use(require("./middlewares/pathNotFound"));
+// app.use(require("./middlewares/pathNotFound"));
 
-app.use(require("./middlewares/error"));
+// app.use(require("./middlewares/error"));
 
 app.listen(process.env.PORT || 3000, () => {
 	console.log("> ✅ server up at port", process.env.PORT);
