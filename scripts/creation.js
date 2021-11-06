aa6643b0c5bf5f1ee335a0f56c46f26f25829c11
Cristina López <82091896+clgsg@users.noreply.github.com>
2021-11-06 13:42:12 +0100

    Feat/configurar db (#3)
    * chore: insert data
    
    * chore: schema and data files re-created and working
    
    Co-authored-by: clgsg <clg_listas@yahoo.es>

--- scripts/creation.js
+++ scripts/creation.js
@@ -5,55 +5,11 @@ const { sql } = require("slonik");
 const creation = async () => {
 	try {
 		await db.query(sql`
-			CREATE type sports AS ENUM('3x3', 'baloncesto', 'fútbol', 'fútbol 7', 'fútbol sala', 'pádel', 'tenis', 'otro');
-			CREATE type locations AS ENUM('Madrid', 'Ávila', 'Segovia'
-			);
-			CREATE type gen AS ENUM('Hombre', 'Mujer');
-			CREATE type game_gen AS ENUM('Masculino', 'Femenino', 'Mixto');
-			CREATE type level AS ENUM('Básico', 'Intermedio', 'Competición');
 
-			CREATE table IF NOT EXISTS users(
-				id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-				username TEXT UNIQUE NOT NULL,
-				first_name TEXT NOT NULL,
-				family_name TEXT NOT NULL,
-				email TEXT UNIQUE NOT NULL,
-				hashed_pwd TEXT NOT NULL,
-				gender gen NOT NULL,
-				birth DATE NOT NULL,
-				u_level level NOT NULL,
-				pref_sports sports[] NOT NULL,
-				profile_pic TEXT,
-				access_token TEXT,
-				activation_token TEXT,
-				created_at TIMESTAMP NOT NULL DEFAULT (now() AT TIME ZONE 'UTC'),
-				updated_at TIMESTAMP NOT NULL DEFAULT (now() AT TIME ZONE 'UTC')
-			);
-			CREATE table IF NOT EXISTS games(
-				id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-				created_by TEXT NOT NULL references users(username),
-				sport sports[] NOT NULL,
-				g_date DATE NOT NULL,
-				g_time TIME NOT NULL,
-				g_level level NOT NULL,
-				g_gender game_gen NOT NULL,
-				created_at TIMESTAMP NOT NULL DEFAULT (now() AT TIME ZONE 'UTC'),
-				updated_at TIMESTAMP NOT NULL DEFAULT (now() AT TIME ZONE 'UTC'),
-				adapted BOOLEAN NOT NULL DEFAULT false,
-				notes TEXT,
-				participants TEXT[]
-				);
-			CREATE table IF NOT EXISTS participants(
-				u_id uuid references users(id),
-				g_id uuid references games(id),
-				CONSTRAINT participants_id PRIMARY KEY (u_id,g_id));
 
-			CREATE TABLE IF NOT EXISTS venues (
-				id uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-				venue_name TEXT NOT NULL,
-				venue_address TEXT NOT NULL,
-				indoor_facilities TEXT,
-				outdoor_facilities TEXT);
+
+
+
 		`);
 		console.info("Creation was successful! 🚀");
 	} catch (error) {
--- scripts/drops.js
+++ scripts/drops.js
@@ -5,19 +5,10 @@ const { sql } = require("slonik");
 const drops = async () => {
 	try {
 		await db.query(sql`
-			DROP table IF EXISTS users CASCADE;
-			DROP table IF EXISTS games CASCADE;
-			DROP table IF EXISTS users_games;
-			DROP table IF EXISTS venues;
 
-			DROP EXTENSION IF EXISTS "uuid-ossp";
-			CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
-			DROP type IF EXISTS sports;
-			DROP type IF EXISTS locations;
-			DROP type IF EXISTS gen;
-			DROP type IF EXISTS game_gen;
-			DROP type IF EXISTS level
+
+
 			`);
 		console.info("Drop was successful! 🚀");
 	} catch (error) {
