0aa18cb91de0718d7bd53b0844bf837f6dd4da27
clgsg <clg_listas@yahoo.es>
2021-11-09 19:01:51 +0100

    chore: query for getGamesByUsersPreferences almost done

--- queries/games.js
+++ queries/games.js
@@ -20,12 +20,14 @@ const getUpcomingGames = async (db) => {
 const getGamesByUsersPreferences = async (db) => {
  	try {
  		const result = await db.query(sql`
- 		SELECT sport, game_date, game_time, game_level, game_venue, adapted
-		FROM games AS g
-		INNER JOIN users AS u
-		ON u.userid =
-		WHERE CAST("sport" AS text) = ${sports} AND adapted = ${adapted} AND game_level = ${game_level}
-         `);
+ 		SELECT g.game_date, g.game_venue, g.game_time
+FROM games as g
+INNER JOIN participants AS p
+ON g.gameid = p.g_id
+INNER JOIN users AS u
+ON u.userid = p.u_id
+WHERE u.user_gender = 'Hombre'
+         `); //TODO: encontrar fórmula para buscar en arrays
  		return result.rows;
  	} catch (error) {
  		console.info("⛔ Error at getGamesByUsersPreferences query: ", error.message);
