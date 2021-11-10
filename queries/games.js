76f312b53e3e08a5e5bada68a13d71ee758d1a61
clgsg <clg_listas@yahoo.es>
2021-11-10 18:41:54 +0100

    feat: games queries done

--- queries/games.js
+++ queries/games.js
@@ -20,14 +20,14 @@ const getUpcomingGames = async (db) => {
 const getGamesByUsersPreferences = async (db) => {
  	try {
  		const result = await db.query(sql`
- 		SELECT g.game_date, g.game_venue, g.game_time
-FROM games as g
-INNER JOIN participants AS p
-ON g.gameid = p.g_id
-INNER JOIN users AS u
-ON u.userid = p.u_id
-WHERE u.user_gender = 'Hombre'
-         `); //TODO: encontrar fórmula para buscar en arrays
+			SELECT g.game_date, g.game_venue, g.game_time, g.game_gender
+			FROM games as g
+			INNER JOIN participants AS p
+			ON g.gameid = p.g_id
+			INNER JOIN users AS u
+			ON u.userid = p.u_id
+			WHERE u.username=${username} AND ${pref_sports}= ANY(u.pref_sports)
+         `);
  		return result.rows;
  	} catch (error) {
  		console.info("⛔ Error at getGamesByUsersPreferences query: ", error.message);
@@ -38,12 +38,11 @@ WHERE u.user_gender = 'Hombre'
  const getGamesByFilter = async (db) => {
 		try {
 			const result = await db.query(sql`
- 		SELECT sport, game_date, game_time, game_level, game_venue, adapted
-		FROM games AS g
-		INNER JOIN users AS u
-		ON u.userid =
-		WHERE CAST("sport" AS text) = ${sports} AND adapted = ${adapted} AND game_level = ${game_level}
+				SELECT sport, game_date, game_time, game_level, game_gender, adapted, game_venue
+				FROM games
+				WHERE game_date=${game_date}::date AND game_time BETWEEN ${game_time} AND '24:00:00' ${sport && `AND sport=${sport}`};
          `);
+
 			return result.rows;
 		} catch (error) {
 			console.info("⛔ Error at getGamesByFilter query: ", error.message);
