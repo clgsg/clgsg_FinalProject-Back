cdd81a70cc439ad4634e0b4656c095eebf6578a8
clgsg <clg_listas@yahoo.es>
2021-11-29 21:53:01 +0100

    chore: added router for removeParticipant service

--- services/participants/index.js
+++ services/participants/index.js
@@ -1,9 +1,9 @@
-
 const router = require("express").Router();
 
 module.exports = (db) => {
 	router.get("/:gameid", require("./getParticipants")(db));
 	router.post("/:gameid/:username", require("./addParticipant")(db));
+	router.delete("/:gameid/:username", require("./removeParticipant"))
 
 	return router;
 };
